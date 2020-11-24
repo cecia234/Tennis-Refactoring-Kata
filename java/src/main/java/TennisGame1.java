ce789a2ce7269e18a97dd7022a6d56af61c49a9c
cecia234 <cecia234@gmail.com>
2020-11-24 10:37:29 +0100

    reformatted code

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,6 +1,6 @@
 
 public class TennisGame1 implements TennisGame {
-    
+
     private int player1Score = 0;
     private int player2Score = 0;
     private String player1Name;
@@ -22,53 +22,49 @@ public class TennisGame1 implements TennisGame {
 
     public String getScore() {
         String overallScore = "";
-        int tempScore=0;
-        if (player1Score ==player2Score)
-        {
-            switch (player1Score)
-            {
+        int tempScore = 0;
+        if (player1Score == player2Score) {
+            switch (player1Score) {
                 case 0:
-                        overallScore = "Love-All";
+                    overallScore = "Love-All";
                     break;
                 case 1:
-                        overallScore = "Fifteen-All";
+                    overallScore = "Fifteen-All";
                     break;
                 case 2:
-                        overallScore = "Thirty-All";
+                    overallScore = "Thirty-All";
                     break;
                 default:
-                        overallScore = "Deuce";
+                    overallScore = "Deuce";
                     break;
-                
+
             }
-        }
-        else if (player1Score >=4 || player2Score>=4)
-        {
-            int pointsDifference = player1Score -player2Score;
-            if (pointsDifference==1) overallScore ="Advantage player1";
-            else if (pointsDifference ==-1) overallScore ="Advantage player2";
-            else if (pointsDifference>=2) overallScore = "Win for player1";
-            else overallScore ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = player1Score;
-                else { overallScore+="-"; tempScore = player2Score;}
-                switch(tempScore)
-                {
+        } else if (player1Score >= 4 || player2Score >= 4) {
+            int pointsDifference = player1Score - player2Score;
+            if (pointsDifference == 1) overallScore = "Advantage player1";
+            else if (pointsDifference == -1) overallScore = "Advantage player2";
+            else if (pointsDifference >= 2) overallScore = "Win for player1";
+            else overallScore = "Win for player2";
+        } else {
+            for (int i = 1; i < 3; i++) {
+                if (i == 1)
+                    tempScore = player1Score;
+                else {
+                    overallScore += "-";
+                    tempScore = player2Score;
+                }
+                switch (tempScore) {
                     case 0:
-                        overallScore+="Love";
+                        overallScore += "Love";
                         break;
                     case 1:
-                        overallScore+="Fifteen";
+                        overallScore += "Fifteen";
                         break;
                     case 2:
-                        overallScore+="Thirty";
+                        overallScore += "Thirty";
                         break;
                     case 3:
-                        overallScore+="Forty";
+                        overallScore += "Forty";
                         break;
                 }
             }
